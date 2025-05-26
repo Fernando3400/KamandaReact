35e79234b72eb0393375034d08f82a11cd9a6a8e
Fernando <fernando.csconceicao@outlook.com>
2025-05-26 16:29:25 -0300

    cabeçalho

--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -16,14 +16,14 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import RenderizadorDeImagem from "./RenderizadorDeImagem";
 import animacao from "../assets/img/animacao.mp4";
 import SearchIcon from "@mui/icons-material/Search";
-
+import { isMobile } from 'react-device-detect';
 import { useEffect, useState, useRef } from "react";
 function Cabecalho(props) {
 
   const tema = createTheme(propiedadesDoTema);
   const token = localStorage.getItem("token")
   const [drawerOpen, setDrawerOpen] = useState(false);
-  
+
 
   const toggleDrawer = (open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -38,16 +38,18 @@ function Cabecalho(props) {
   const videoRef = useRef(null);
   const estaLogado = usuario && usuario !== "null" && token !== "null";
   const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+  const [alternarMenu, setAlternarMenu] = useState(false);
+
   const [showSearch, setShowSearch] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const handleClick = (event) => {
+
+  const fecharMenu = () => {
+    setAnchorEl(null);
+  };
+  const definirPontoDeAncora = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -57,7 +59,7 @@ function Cabecalho(props) {
         videoRef.current.play();
       }
     };
-
+    console.log(isMobile)
     playVideo();
 
     const interval = setInterval(playVideo, 8000);
@@ -76,61 +78,122 @@ function Cabecalho(props) {
 
   return (
     <ThemeProvider theme={tema}>
-      <AppBar sx={{ height: "10vh" }} position="sticky" style={{ backgroundColor: tema.palette.primary.main }}>
-        <Toolbar
-          sx={{
-            display: "flex",
-            flexWrap: "wrap", // permite quebra de linha
-            alignItems: "center",
-            justifyContent: "space-between",
-            rowGap: 1, // espaço vertical entre linhas
-          }}
-        >
-          {/* Linha Esquerda */}
+      {
+        isMobile ? (
           <Stack
-            direction="row"
-            sx={{
-              flexWrap: "wrap",
-              alignItems: "center",
-              gap: 2,
-              flexGrow: 1,
-              minWidth: 0,
-            }}
+            justifyContent="center"
+            alignItems="center"
+            width="100%"
+            height="100vh"
+            padding="5px"
+            sx={{ backgroundColor: "black" }}
+            direction="column"
           >
-            <IconButton
-              disableRipple
-              color="inherit"
-              onClick={() => {
-                navigate("/");
+            <Stack>
+              {/* Espaço reservado caso queira adicionar algo */}
+            </Stack>
+
+            <video ref={videoRef} height="60px" width="200px" muted>
+              <source src={animacao} type="video/mp4" />
+              Seu navegador não suporta vídeos.
+            </video>
+
+            <Stack direction="row" justifyContent="center" alignItems="center" marginTop="10px">
+              {!estaLogado && (
+                <Button color="secondary" variant="outlined" onClick={() => navigate("portal/login")}>
+                  <Typography color="white" textTransform="none">Entrar</Typography>
+                </Button>
+              )}
+              {estaLogado && (
+                <Stack
+                  onClick={(e) => {
+                    setAlternarMenu(!alternarMenu)
+                    definirPontoDeAncora(e)
+                  }}
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Typography color="white" variant="h6">{usuario}</Typography>
+                  <IconButton color="inherit">
+                    <AccountCircleIcon />
+                  </IconButton>
+                  <Menu
+                    anchorEl={anchorEl}
+                    open={alternarMenu}
+                    onClose={() => {
+                      setAlternarMenu(!alternarMenu)
+                    }
+                    }
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    transformOrigin={{ vertical: "top", horizontal: "right" }}
+                  >
+                    <MenuItem onClick={() => navigate("/loja/meuspedidos")}>Meus Pedidos</MenuItem>
+                    <MenuItem onClick={() => {
+                      localStorage.removeItem("token");
+                      localStorage.removeItem("usuario");
+                      navigate("/");
+                      window.location.reload();
+                    }}>
+                      Sair
+                    </MenuItem>
+                  </Menu>
+                </Stack>
+              )}
+            </Stack>
+          </Stack>) : (<AppBar sx={{}} position="sticky" style={{ backgroundColor: tema.palette.primary.main }}>
+            <Toolbar
+              sx={{
+                display: "flex",
+                flexWrap: "wrap", // permite quebra de linha
+                alignItems: "center",
+                justifyContent: "space-between",
+                rowGap: 1, // espaço vertical entre linhas
               }}
             >
-              <Stack direction={"column"} alignItems={"center"} justifyContent={"center"}>
-                <video ref={videoRef} height="60px" width="200px" muted>
-                  <source src={animacao} type="video/mp4" />
-                  Seu navegador não suporta vídeos.
-                </video>
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  onClick={() => setDialogoKamanda(true)}
+              {/* Linha Esquerda */}
+              <Stack
+                direction="row"
+                sx={{
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  gap: 2,
+                  flexGrow: 1,
+                  minWidth: 0,
+                }}
+              >
+                <IconButton
+                  disableRipple
+                  color="inherit"
+                  onClick={() => {
+                    navigate("/");
+                  }}
                 >
-                  <Typography
-                    width={"100px"}
-                    textTransform="none"
-                    color={"white"}
-                    fontSize={10}
-                  >
-                    O que é kamanda
-                  </Typography>
-                </Button>
-              </Stack>
-            </IconButton>
+                  <Stack direction={"column"} alignItems={"center"} justifyContent={"center"}>
+                    <video ref={videoRef} height="60px" width="200px" muted>
+                      <source src={animacao} type="video/mp4" />
+                      Seu navegador não suporta vídeos.
+                    </video>
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      onClick={() => setDialogoKamanda(true)}
+                    >
+                      <Typography
+                        width={"100px"}
+                        textTransform="none"
+                        color={"white"}
+                        fontSize={10}
+                      >
+                        O que é kamanda
+                      </Typography>
+                    </Button>
+                  </Stack>
+                </IconButton>
 
-            <IconButton onClick={() => setShowSearch(!showSearch)} color="secondary">
-              <SearchIcon />
-            </IconButton>
 
-            <Collapse
+
+                {/* <Collapse
               in={showSearch}
               orientation="horizontal"
               sx={{
@@ -169,90 +232,85 @@ function Cabecalho(props) {
                   }
                 }}
               />
-            </Collapse>
-          </Stack>
+            </Collapse> */}
+              </Stack>
 
-          {/* Linha Direita (vai pra linha de baixo em telas pequenas) */}
-          <Typography
-            variant="h6"
-            color="white"
-            sx={{
-              mt: { xs: 1, sm: 0 }, // margin top só em telas pequenas
-              width: { xs: "100%", sm: "auto" }, // quebra em 100% no mobile
-              textAlign: { xs: "center", sm: "right" },
-            }}
-          >
-            {props.nome}
-          </Typography>
-
-          <Dialog open={dialogoKamanda}>
-            <DialogContent>
-              <Typography  >Kamanda significa "Camarada". É como os falantes de refenciavam as pessoas que tinham afinidade intelectual, assim como chamamos Camarada ou Companheiro aqui no Brasil.
+              {/* Linha Direita (vai pra linha de baixo em telas pequenas) */}
+              <Typography
+                variant="h6"
+                color="white"
+                sx={{
+                  mt: { xs: 1, sm: 0 }, // margin top só em telas pequenas
+                  width: { xs: "100%", sm: "auto" }, // quebra em 100% no mobile
+                  textAlign: { xs: "center", sm: "right" },
+                }}
+              >
+                {props.nome}
               </Typography>
-              <Typography marginTop={"5px"} > Swahili, um idioma Bantu (análogo ao Latim), é o terceiro idioma mais falado no continente africano, atrás do Inglês e o Francês. Logo,
-                a referencia ao nome "Kamanda" é um resgate a um fragmento das raízes do povo negro
-              </Typography>
-            </DialogContent>
-            <DialogActions>
-              <Button variant="outline" onClick={() => {
-                setDialogoKamanda(false)
-              }}>Fechar</Button></DialogActions>
-          </Dialog>
 
-          {/* Nome do usuário e botões à direita (peso médio) */}
+              <Dialog open={dialogoKamanda}>
+                <DialogContent>
+                  <Typography  >Kamanda significa "Camarada". É como os falantes de refenciavam as pessoas que tinham afinidade intelectual, assim como chamamos Camarada ou Companheiro aqui no Brasil.
+                  </Typography>
+                  <Typography marginTop={"5px"} > Swahili, um idioma Bantu (análogo ao Latim), é o terceiro idioma mais falado no continente africano, atrás do Inglês e o Francês. Logo,
+                    a referencia ao nome "Kamanda" é um resgate a um fragmento das raízes do povo negro
+                  </Typography>
+                </DialogContent>
+                <DialogActions>
+                  <Button variant="outline" onClick={() => {
+                    setDialogoKamanda(false)
+                  }}>Fechar</Button></DialogActions>
+              </Dialog>
 
-          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", px: 2 }}>
-            {/* Botões à esquerda */}
+              {/* Nome do usuário e botões à direita (peso médio) */}
 
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", px: 2 }}>
 
 
-              <Box sx={{ display: "flex", alignItems: "center", position: "relative" }}>
 
+                {/* Usuário / Login / Menu */}
+                <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-around", gap: 2 }}>
+                  {usuario ? (
+                    <Typography variant="h6">{usuario}</Typography>
+                  ) : (
+                    <Button color="secondary" sx={{ px: "20px" }} variant="contained" onClick={() => navigate("portal/login")}>
+                      <Typography color="black" textTransform="none">Entrar</Typography>
+                    </Button>
+                  )}
+
+                  {estaLogado && (
+                    <>
+                      <IconButton color="inherit" onClick={() => setAlternarMenu(!alternarMenu)}>
+                        <AccountCircleIcon />
+                      </IconButton>
+                      <Menu
+                        anchorEl={anchorEl}
+                        open={alternarMenu}
+                        onClose={() => setAlternarMenu(false)}
+                        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                        transformOrigin={{ vertical: "top", horizontal: "right" }}
+                      >
+                        { }
+                        <MenuItem onClick={() => navigate("/loja/meuspedidos")}>Meus Pedidos</MenuItem>
+                        <MenuItem onClick={() => navigate("/portal/estabelecimentos/minhaloja")}>Area de Estabelecimentos</MenuItem>
+                        <MenuItem onClick={() => {
+                          localStorage.removeItem("token");
+                          localStorage.removeItem("usuario");
+                          navigate("/");
+                          window.location.reload();
+                        }}>
+                          Sair
+                        </MenuItem>
+                      </Menu>
+                    </>
+                  )}
+                </Box>
               </Box>
-            </Box>
+            </Toolbar>
+          </AppBar>)
+      }
 
-            {/* Usuário / Login / Menu */}
-            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-              {usuario ? (
-                <Typography variant="h6">{usuario}</Typography>
-              ) : (
-                <Button color="secondary" sx={{ px: "20px" }} variant="contained" onClick={() => navigate("portal/login")}>
-                  <Typography color="black" textTransform="none">Entrar</Typography>
-                </Button>
-              )}
-
-              {estaLogado && (
-                <>
-                  <IconButton color="inherit" onClick={handleClick}>
-                    <AccountCircleIcon />
-                  </IconButton>
-                  <Menu
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                    transformOrigin={{ vertical: "top", horizontal: "right" }}
-                  >
-                    {}
-                    <MenuItem onClick={() => navigate("/loja/meuspedidos")}>Meus Pedidos</MenuItem>
-                    <MenuItem onClick={() => navigate("/portal/estabelecimentos/minhaloja")}>Area de Estabelecimentos</MenuItem>
-                    <MenuItem onClick={() => {
-                      localStorage.removeItem("token");
-                      localStorage.removeItem("usuario");
-                      navigate("/");
-                      window.location.reload();
-                    }}>
-                      Sair
-                    </MenuItem>
-                  </Menu>
-                </>
-              )}
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 };
 
